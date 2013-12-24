0425a15e8b48ad7e282fd52779ccfc28d80163fa
UENISHI Kota <kota@basho.com>
2013-12-24 18:58:12 +0900

    start bucket typing

--- bin/setup_index.sh
+++ bin/setup_index.sh
@@ -5,8 +5,8 @@ HOST=$1
 echo setting up md_index at $HOST
 curl -X PUT http://${HOST}/search/index/md_index
 echo "setting relation md_index to ssmix, rezept bucket"
-curl -X PUT http://${HOST}/buckets/ssmix/props -H 'Content-type:application/json' -d '{"props":{"yz_index":"md_index"}}'
-curl -X PUT http://${HOST}/buckets/rezept/props -H 'Content-type:application/json' -d '{"props":{"yz_index":"md_index"}}'
+curl -X PUT http://${HOST}/buckets/ssmix/props -H 'Content-type:application/json' -d '{"props":{"search_index":"md_index"}}'
+curl -X PUT http://${HOST}/buckets/rezept/props -H 'Content-type:application/json' -d '{"props":{"search_index":"md_index"}}'
 
 curl http://${HOST}/search/index?index=true
 curl http://${HOST}/buckets/ssmix/props
--- bin/yz_checker.sh
+++ bin/yz_checker.sh
@@ -11,12 +11,12 @@ echo "HTTP_PROXY"=$HTTP_PROXY
 echo "HTTPS_PROXY"=$HTTPS_PROXY
 
 echo creating index "test_index" ...
-curl -i -X PUT http://${HOST}/yz/index/test_index
+curl -i -X PUT http://${HOST}/search/index/test_index
 
 echo setting index to test_bucket ...
 R=`curl -i -X PUT http://${HOST}/buckets/test_bucket/props \
   -H 'Content-type:application/json' \
-  -d '{"props":{"yz_index":"test_index"}}' | head -n 1`
+  -d '{"props":{"search_index":"test_index"}}' | head -n 1`
 echo $R
 
 R=`curl http://${HOST}/buckets/test_bucket/props`
@@ -27,7 +27,7 @@ sleep 1
 echo putting test data...
 R=`curl -i -X PUT http://${HOST}/buckets/test_bucket/keys/test \
   -H 'Content-type:application/json' \
-  -d '{"props":{"yz_index":"test_index"}}' | head -n 1`
+  -d '{"props":{"search_index":"test_index"}}' | head -n 1`
 echo $R
 
 sleep 1
@@ -39,7 +39,7 @@ echo $R
 curl -X DELETE http://${HOST}/buckets/test_bucket/keys/test
 curl -X DELETE http://${HOST}/buckets/test_bucket/props \
   -H 'Content-type:application/json' \
-  -d '{"props":{"yz_index":"test_index"}}'
+  -d '{"props":{"search_index":"test_index"}}'
 curl -X DELETE http://${HOST}/search/index/test_index
 
 echo "\nAll tests ran successfully."
