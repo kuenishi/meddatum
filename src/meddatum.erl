28fb349a40ac0d912f0c02642a198f769ed0ec78
UENISHI Kota <kota@basho.com>
2013-12-30 14:17:36 +0900

    update to bucket types, at recept codes

--- src/meddatum.erl
+++ src/meddatum.erl
@@ -16,85 +16,11 @@
 
 -module(meddatum).
 
--export([search/2, search/3, true_bucket_name/1, maybe_new_ro/5,
+-export([true_bucket_name/1, maybe_new_ro/5,
          setup/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 -include("meddatum.hrl").
-
-search(Server, Query) ->
-    search(Server, Query,
-           fun md_searcher:simple_doc_retriever/1).
-
-search(Server, Query, DocRetriever) ->
-
-    Self = self(),
-    Pid = spawn_link(fun() -> consumer(Server, waiting, Self) end),
-    ibrowse:start(),
-    io:setopts([{encoding, unicode}]),
-    Url = md_searcher:plain_query_url(Server, Query),
-    {ok, {NumFound, BKs}} = md_searcher:run_query(Url, 0, DocRetriever),
-    _ = lager:info("sending query to ~p~n", [Url]),
-    Pid ! {start, NumFound, BKs},
-
-    _ = lager:info("~p results found (~p).~n", [NumFound, length(BKs)]),
-
-    pagenate(Url, NumFound, length(BKs), Pid, DocRetriever),
-    
-    ibrowse:stop(),
-
-    receive done ->
-            _ = lager:info("all data yeilded.~n")
-    end.
-
-
-pagenate(Url0, _NumFound0, Offset, Pid, DocRetriever) ->
-    {ok, {NumFound, BKs}} = md_searcher:run_query(Url0, Offset, DocRetriever),
-
-    if length(BKs) > 0 ->
-            Pid ! {data, BKs},
-            pagenate(Url0, NumFound, Offset+length(BKs), Pid, DocRetriever);
-       true ->
-            ok
-    end.
-
-consumer(Server, waiting, Parent) ->
-    {ok, C} = riakc_pb_socket:start_link(Server, 8087),
-    receive
-        {start, NumFound, Docs} ->
-            io:format(standard_error, "start processing ~p/~p records found.~n",
-                      [length(Docs), NumFound]),
-            lists:foreach(fun(Doc) -> print_doc(C, Doc) end, Docs),
-            consumer(C, {waiting, NumFound-length(Docs)}, Parent)
-    end;
-consumer(C, {waiting, Remain}, Parent) when Remain > 0 ->
-    receive
-        {data, Docs} ->
-            io:format(standard_error, "processing ~p/~p records found.~n",
-                      [length(Docs), Remain]),
-            lists:foreach(fun(Doc) -> print_doc(C, Doc) end, Docs),
-            consumer(C, {waiting, Remain-length(Docs)}, Parent)
-    end;
-consumer(C, {waiting, _}, Parent) ->
-    %% done
-    riakc_pb_socket:stop(C),
-    Parent ! done.
-
-print_doc(C, {B,K}) ->
-    {ok, RiakObj} = riakc_pb_socket:get(C, B, K),
-    V = riakc_obj:get_value(RiakObj),
-    io:format(standard_error, "[info] ~p ~p (~p bytes)~n", [B, K, byte_size(V)]),
-    case unicode:characters_to_list(V, utf8) of
-        E when is_tuple(E) ->
-            {error, _, T} = E,
-            io:format(standard_error, "error ~p~n", [binary_to_term(T)]),
-            exit(E);
-        String when is_list(String) ->
-            io:put_chars(String),
-            io:format("~n");
-        Other ->
-            io:format(standard_error, "unknown: ~p~n", [Other])
-    end.
 
 true_bucket_name(Bucket0) ->
     case meddatum_config:use_bucket_types() of
