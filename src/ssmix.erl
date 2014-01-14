06cccb5acb48456f5ef4504f1db324d9f0d74afc
UENISHI Kota <kota@basho.com>
2014-01-14 15:27:39 +0900

    remove ssmix_walker; change hl7parser to parse single hl7 file

--- src/ssmix.erl
+++ src/ssmix.erl
@@ -16,12 +16,9 @@
 
 -module(ssmix).
 
--export([walk/1, walk/3, walk2/4]).
+-export([walk2/4]).
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("meddatum/include/hl7.hrl").
-
-walk(Path) ->
-    walk(Path, localhost, 8087).
 
 walk2(Path, HospitalID, Host, Port) ->
     {ok,C}=ssmix_importer:connect(Host, Port),
@@ -44,31 +41,3 @@ walk2(Path, HospitalID, Host, Port) ->
         end,
     _ErrorFiles = filelib:fold_files(Path, "", true, F, []),
     ok=ssmix_importer:disconnect(C).
-
-
-walk(Path, Host, Port) ->
-    {ok, Pid} = ssmix_walker:start_link(Path),
-    {ok,C}=ssmix_importer:connect(Host, Port),
-    _ = lager:info("connected.~p~n", [C]),
-    timer:sleep(100),
-    wait_for_walker(Pid, C, 0).
-
-wait_for_walker(Pid, C, N) ->
-    {Flag, HL7Msgs} = ssmix_walker:pop(Pid),
-
-    lager:info("inserting ~p msgs..~n", [length(HL7Msgs)]),
-    lists:foreach(fun(HL7Msg) ->
-                          lager:info("inserting:~n", []),
-			  try
-                  ok=ssmix_importer:put_json(C, HL7Msg)
-			  catch T:E ->
-                      _ = lager:error("~p:~p", [T,E])
-              end,
-                          lager:info("done.~n", [])
-                  end, HL7Msgs),
-    N2 = length(HL7Msgs) + N,
-    lager:info("~p msgs stored.~n", [length(HL7Msgs)]),
-    case Flag of
-        ok -> ok=ssmix_importer:disconnect(C);
-        cont -> wait_for_walker(Pid, C, N2)
-    end.
