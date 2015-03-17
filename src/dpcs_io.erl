92bbb5493642c5abca5b15b31380136d117c3eb1
UENISHI Kota <kota@basho.com>
2015-03-17 22:26:56 +0900

    More Erlang-ish way

--- src/dpcs_io.erl
+++ src/dpcs_io.erl
@@ -1,15 +1,14 @@
 -module(dpcs_io).
 
--export([put_record/5]).
-
--include_lib("eunit/include/eunit.hrl").
+-include("meddatum.hrl").
 -include_lib("riakc/include/riakc.hrl").
 
-put_record(C , Mode, HospitalID, Date , {Key, CommonFields, RececdFields}) ->
+-export([put_record/5]).
+
+put_record(C, Mode, HospitalID, _Date, {Key, CommonFields, RececdFields}) ->
     ContentType = "application/json",
     BucketName = Mode ++ ":" ++ HospitalID,
-    Bucket = {<<"md">> , list_to_binary(BucketName)},
+    Bucket = {?BUCKET_TYPE , list_to_binary(BucketName)},
     JSONRecords = jsone:encode({CommonFields ++ RececdFields}, [native_utf8]),
     RiakObj = meddatum:maybe_new_ro(C, Bucket, list_to_binary(Key), JSONRecords, ContentType),
-    riakc_pb_socket:put(C, RiakObj),
-    ok.
+    riakc_pb_socket:put(C, RiakObj).
