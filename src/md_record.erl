d2f591afc67166429c1131a2b3f1cbe7c3e4da86
UENISHI Kota <kota@basho.com>
2015-03-17 23:11:46 +0900

    Add xref and dialyzer check

--- src/md_record.erl
+++ src/md_record.erl
@@ -16,22 +16,36 @@
 
 -module(md_record).
 
--type md_record() :: tuple(). %% record data type
-
 -include("meddatum.hrl").
--export([bucket2hospital_id/1]).
+-export([bucket2hospital_id/1,
+         new/3, put_record/2]).
+
+-record(md_record,
+        {module :: module(),
+         payload :: term(),
+         type :: atom()}).
+
+-type md_record(_) :: #md_record{}.
+-export_type([md_record/1]).
 
 %% note that the file is usually *NOT* JSON.
--callback from_file(filename:filename(), list(), pid()) -> {ok, [md_record()]}.
--callback from_file(filename:filename(), list(), pid(), PostProcessor::fun()) -> {ok, [md_record()]}.
+-callback from_file(filename:filename(), list(), pid()) -> {ok, [md_record(_)]}.
+-callback from_file(filename:filename(), list(), pid(), PostProcessor::fun()) -> {ok, [md_record(_)]}.
 
--callback to_json(md_record()) ->  {ok, JSON::binary()}.
--callback from_json(JSON::binary()) -> md_record().
--callback key(md_record()) -> binary().
--callback bucket(md_record()) -> binary().
--callback patient_id(md_record()) -> binary().
--callback hospital_id(md_record()) -> binary().
+-callback to_json(md_record(_)) ->  {ok, JSON::binary()}.
+-callback from_json(JSON::binary()) -> md_record(_).
+-callback key(md_record(_)) -> binary().
+-callback bucket(md_record(_)) -> binary().
+-callback patient_id(md_record(_)) -> binary().
+-callback hospital_id(md_record(_)) -> binary().
+-callback payload(md_record(T)) -> T.
 -callback columns() -> JSON::binary().
+
+new(Module, Payload, Type) ->
+    #md_record{module=Module, payload=Payload, type=Type}.
+
+-spec put_record(pid(), md_record(_)) -> ok.
+put_record(_, _) -> undefined.
 
 bucket2hospital_id({_, Bucket}) -> bucket2hospital_id(Bucket);
 bucket2hospital_id(Bucket) when is_binary(Bucket) ->
